4557d9bf97f5a25d99d0562cef4f4f36468f28f7
David Kling <drkling2000@gmail.com>
2019-02-26 10:29:34 -0500

    completed main.py for pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,19 +1,47 @@
 import os
 import csv
-budgetcsv = os.path.join ("resources","budget_data.csv")
-with open(budgetcsv, 'r') as csvfile:
+csvpath = os.path.join ("resources","budget_data.csv")
+rowcount = 0
+total = 0
+totalpnl_change = 0
+pnl_change = 0
+pnl = 0
+max = 0
+min = 0
+maxmonth = ""
+with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-
+    next(csvreader) 
 # The total number of months included in the dataset
-    next(csvreader)
-    row_count = sum(1 for row in csvreader)
-    print(row_count)
-
-
-  # The net total amount of "Profit/Losses" over the entire period
-
-  # The average of the changes in "Profit/Losses" over the entire period
-
-  # The greatest increase in profits (date and amount) over the entire period
-
-  # The greatest decrease in losses (date and amount) over the entire period
+    for row in csvreader:
+        rowcount = rowcount + 1
+        if rowcount == 1:
+            pnl = int(row[1])
+        total = total + int(row[1])
+        pnl_change = int(row[1]) - pnl
+        pnl = int(row[1])
+        if max < pnl_change:
+            max = pnl_change
+            maxmonth = row[0]
+        if min > pnl_change:
+            min = pnl_change
+            minmonth = row[0]
+        totalpnl_change += pnl_change
+    avgchange = totalpnl_change / (rowcount - 1)
+    avg = round(avgchange,2)
+    print ('Financial Analysis')
+    print('-----------------------------')
+    print ('Total months: ' + str(rowcount))
+    print ('Total: $' + str(total)) 
+    print ('Average Change: $' +str(avg))
+    print('Greatest increase in profits: ' + maxmonth + ' '+ '($' + str(max) + ")")
+    print('Greatest decrease in profits: ' + minmonth + ' '+ '($' + str(min) +')')
+    
+output_path = os.path.join("resources", "results.csv")
+with open(output_path, 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',')
+    csvwriter.writerow(['Category','result'])
+    csvwriter.writerow(['Total Months', str(rowcount)])
+    csvwriter.writerow(['Average Change', str(avg)])
+    csvwriter.writerow(['Greatest increase in profts', maxmonth + ' ' +str(max)])
+    csvwriter.writerow(['Greatest decrease in profts', minmonth + ' ' +str(min)])
